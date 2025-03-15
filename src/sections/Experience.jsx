76ce6fbe1bc518d8e1af2355decb81d0eb5afde0
Swayam Shalgar <swayamshalgar1211@gmail.com>
2025-03-15 18:26:39 +0530

    Update3

--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -12,7 +12,7 @@ const WorkExperience = () => {
   return (
     <section className="c-space my-20" id="work">
       <div className="w-full text-white-600">
-        <p className="head-text">My Education Journey</p>
+        <p className="head-text">My Tech Skills</p>
 
         <div className="work-container">
           <div className="work-canvas">
